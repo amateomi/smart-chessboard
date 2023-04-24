c5fe7b983acb64f7221cb899b966e2c8d505affa
Andrey Sikorin <amaomi.prog@gmail.com>
2023-04-24 22:31:13 +0300

    Move all constants in one module

---
+++ constants.py
@@ -0,0 +1,43 @@
+from chess import parse_square, Move, Piece, QUEEN, ROOK, BISHOP, KNIGHT
+
+TOTAL_SQUARES = 64
+
+# All possible castling squares for the player
+# NOTE: Assumed that the player always plays for the white pieces
+TARGET_CASTLING_SQUARES = [parse_square("c1"),
+                           parse_square("g1")]
+
+# List of all possible player moves that may lead to pawn promotion
+# NOTE: Assumed that the player always plays for the white pieces
+PAWN_PROMOTION_MOVES = [Move.from_uci("a7a8"),
+                        Move.from_uci("a7b8"),
+
+                        Move.from_uci("b7a8"),
+                        Move.from_uci("b7b8"),
+                        Move.from_uci("b7c8"),
+
+                        Move.from_uci("c7b8"),
+                        Move.from_uci("c7c8"),
+                        Move.from_uci("c7d8"),
+
+                        Move.from_uci("d7c8"),
+                        Move.from_uci("d7d8"),
+                        Move.from_uci("d7e8"),
+
+                        Move.from_uci("e7d8"),
+                        Move.from_uci("e7e8"),
+                        Move.from_uci("e7f8"),
+
+                        Move.from_uci("f7e8"),
+                        Move.from_uci("f7f8"),
+                        Move.from_uci("f7g8"),
+
+                        Move.from_uci("g7f8"),
+                        Move.from_uci("g7g8"),
+                        Move.from_uci("g7h8"),
+
+                        Move.from_uci("h7g8"),
+                        Move.from_uci("h7h8")]
+
+PAWN_PROMOTION_STRING_OPTIONS = ["Queen", "Rook", "Bishop", "Knight"]  # Used to print options to player
+PAWN_PROMOTION_OPTIONS: list[Piece] = [QUEEN, ROOK, BISHOP, KNIGHT]  # Used to set promotion for a move
